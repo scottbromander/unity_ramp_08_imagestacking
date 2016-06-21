63dff727b532786ce31a5d984d598bf2e17f2178
Scott Bromander <scott@primeacademy.io>
2016-06-21 15:51:37 -0500

    ininitial commit

---
+++ Assets/ArrangeActions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ArrangeActions : MonoBehaviour {
+	private RectTransform panelRectTransform;
+
+	// Use this for initialization
+	void Start () {
+		panelRectTransform = GetComponent<RectTransform>();
+	}
+
+	public void MoveDownOne(){
+		int currentSiblingIndex = panelRectTransform.GetSiblingIndex ();
+		print ("(before change) " + panelRectTransform.name + " sibling index =  " + currentSiblingIndex);
+		panelRectTransform.SetSiblingIndex (currentSiblingIndex - 1);
+		print ("After Change: " + panelRectTransform.name + " sibling index = " + currentSiblingIndex);
+	}
+
+	public void MoveUpOne(){
+		int currentSiblingIndex = panelRectTransform.GetSiblingIndex ();
+		print ("Before Change: " + panelRectTransform.name + " sibling index = " + currentSiblingIndex);
+		panelRectTransform.SetSiblingIndex (currentSiblingIndex + 1);
+		print ("After Change: " + panelRectTransform.name + " sibling index = " + currentSiblingIndex);
+	}
+}
